9c2e1c4f9df1bfe81a1d9aea47b62d25f26758f1
jpcordebello <cordebello.jp@gmail.com>
2026-08-11 16:37:20 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/br/uff/gerenciadoreventos/GerenciadorEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uff.gerenciadoreventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
